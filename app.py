ad2eb4458d305d8943c0c7b9013b9a8912005c61
rahiq-h123 <77285448+rahiq-h123@users.noreply.github.com>
2023-09-06 04:20:26 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,55 +1,16 @@
-from flask import Flask, render_template, request
-import pandas as pd
-from io import StringIO
-import pickle
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-# Load the pre-trained machine learning model
-model = pickle.load(open('best_model.pkl','rb'))
-encoder = pickle.load(open('encoder.pkl','rb'))
-scaler = pickle.load(open('scaler.pkl','rb'))
-top_50_feature_names = pickle.load(open('top_50_feature_names.pkl','rb'))
+@app.route('/upload', methods=['POST'])
+def upload():
+    contents = request.json['contents'] # Get the file contents from the request
 
-@app.route('/')
-def index():
-    return render_template('application.html')
+    # Process the CSV file and extract the desired feature
+    # Replace this with your own logic
+    feature = contents.split(',')[6]
 
-@app.route('/predict', methods=['POST'])
-def predict():
-    # Retrieve the CSV data from the request
-    csv_data = request.json['csvData']
-
-    # Convert the CSV data to a Pandas DataFrame
-    df = pd.read_csv(StringIO(csv_data))
-    
-    #data preprocessing
-    df = df.fillna(df.mean())
-    df['sex']= encoder.transform(df[['sex']])
-    df['specific.disorder']= encoder.transform(df[['specific.disorder']])
-    df = df.drop(columns =['no.', 'eeg.date', 'Unnamed: 122'], axis =1)
-    df1=[]
-    for j in list(top_50_feature_names):
-        df1.append(df[j])
-    df1 = pd.concat(df1, axis = 1)
-    df1 = scaler.transform(df1)
-
-    # Make predictions using the pre-trained model
-    predictions = model.predict(df1)
-    case_mapping = {
-    0: "Addictive disorder",
-    1: "Trauma and stress related disorder",
-    2: "Mood disorder",
-    3: "Healthy control",
-    4: "Obsessive compulsive disorder",
-    5: "Schizophrenia",
-    6: "Anxiety disorder"
-                 }
-
-    output = case_mapping[predictions] 
-
-    # Return the prediction results as a string
-    return 'Prediction result: ' + output
+    return jsonify({'feature': feature})
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host="0.0.0.0", port=5000)
